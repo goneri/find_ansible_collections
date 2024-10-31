014e6e25a4c2e57d47e238349df7936679b34c98
Gonéri Le Bouder <goneri@lebouder.net>
2024-10-31 19:57:19 -0400

    scan collection or playbook projects

--- find_collection.ts
+++ find_collection.ts
@@ -1,57 +1,136 @@
-import * as path from 'path';
+import * as path from "path";
+import * as YAML from "yaml";
 
-import { Dirent, Stats, readdir } from 'fs';
-import { stat, } from 'node:fs/promises';
+import { Dir, Dirent, Stats } from "fs";
+import { readdir, readFile } from "node:fs/promises";
 
-const directoryPath = "/var/home/goneri/git_repos/cloud-init-bsd-testing/";
-const collectionsPath = directoryPath + "collections/ansible_collections/";
+class AnsibleCollection {
+  path: string;
+  namespace: string;
+  name: string;
 
-
-function namespaceCallback(err, dirents: Dirent[]) {
-    dirents
-        .filter((dirent) => dirent.isDirectory())
-        .map((dirent) => {
-            const namespaceName = dirent.name;
-            const namespaceDirectory = path.join(dirent.parentPath, namespaceName);
-            let collectionCallback = (error, dirents: Dirent[]) => {
-                dirents
-                    .filter((dirent) => dirent.isDirectory())
-                    .map((dirent) => {const manifestFile = path.join(dirent.parentPath, dirent.name, "MANIFEST.json"); return {dirent, manifestFile}})
-                    .map(({dirent, manifestFile}) =>{console.log(`${namespaceName}.${dirent.name}`);});
-            }
-
-
-
-            readdir(namespaceDirectory, {withFileTypes: true}, collectionCallback);
-
-
-        });
+  constructor(path, namespace, name) {
+    this.path = path;
+    this.namespace = namespace;
+    this.name = name;
+  }
 }
 
+class CollectionFinder {
+  cache: AnsibleCollection[];
+  workspacePath: string;
 
+  constructor(workspacePath: string) {
+    this.workspacePath = workspacePath;
+  }
 
-readdir(collectionsPath, {withFileTypes: true}, namespaceCallback);
+  async readCollectionMetaInformation(collectionPath: string) {
+    const galaxyFile = path.join(collectionPath, "galaxy.yml");
+    let fromGalaxyFile = readFile(galaxyFile, "utf8")
+      .then((content) => YAML.parse(content))
+      .then((info) => {
+        return { namespace: info["namespace"], name: info["name"] };
+      });
 
-//        files.filter((file)) => { nodeFsPromise.stat(collectionsPath + file).then((statInfo) => {})) });
-        //listing all files using forEach
+    const MANIFESTFile = path.join(collectionPath, "MANIFEST.json");
+    let fromMANIFESTFile = readFile(MANIFESTFile, "utf8")
+      .then((content) => JSON.parse(content))
+      .then((info) => {
+        return {
+          namespace: info["collection_info"]["namespace"],
+          name: info["collection_info"]["name"],
+        };
+      });
 
+    const j = Promise.any([fromGalaxyFile, fromMANIFESTFile])
+      .then((info) => {
+        const shouldEndWith = `${info["namespace"]}/${info["name"]}`;
+        if (!collectionPath.endsWith(`${info["namespace"]}/${info["name"]}`)) {
+          console.log(
+            `collect name and path mismatch: ${collectionPath} Vs ${shouldEndWith}`,
+          );
+        }
+        return new AnsibleCollection(
+          collectionPath,
+          info["namespace"],
+          info["name"],
+        );
+      })
+      .catch((e) => {
+        console.debug(
+          `Cannot find collection meta information in directory ${collectionPath}`,
+        );
+        return null;
+      });
+    return j;
+  }
 
+  async searchNestedCollections() {
+    const collectionsPath = path.join(
+      this.workspacePath,
+      "collections/ansible_collections/",
+    );
+    let a = await readdir(collectionsPath, { withFileTypes: true })
+      .then((namespaceDirectories: Dirent[]) => {
+        return namespaceDirectories
+          .filter((namespaceEntry) => namespaceEntry.isDirectory())
+          .map((namespaceEntry) => {
+            const namespaceName = namespaceEntry.name;
+            const namespaceDirectory = path.join(
+              namespaceEntry.parentPath,
+              namespaceName,
+            );
+            return readdir(namespaceDirectory, { withFileTypes: true }).then(
+              (collectionDirectories: Dirent[]) => {
+                return collectionDirectories
+                  .filter((entry) => entry.isDirectory())
+                  .map((entry) =>
+                    this.readCollectionMetaInformation(
+                      path.join(entry.path, entry.name),
+                    ),
+                  );
+              },
+            );
+          });
+      })
+      .catch((e) => {
+        console.debug(`Cannot open directory ${collectionsPath}`);
+        return [];
+      });
+    let r: AnsibleCollection[] = [];
+    for (let i = 0; i < a.length; i++) {
+      (await Promise.all(await a[i])).forEach((entry) => {
+        if (entry instanceof AnsibleCollection) {
+          r.push(entry);
+        }
+      });
+    }
+    return r;
+  }
 
-    // forEach((namespaceDirectory) => {
-    //         // Do whatever you want to do with the file
-    //         console.log(namespaceDirectory);
+  async refreshCache() {
+    let scanCollectionWorkspace = await this.readCollectionMetaInformation(
+      this.workspacePath,
+    );
+    let scanPlaybookWorkspace = await this.searchNestedCollections();
+    if (scanCollectionWorkspace) {
+      this.cache = [scanCollectionWorkspace];
+    } else {
+      this.cache = scanPlaybookWorkspace;
+    }
+    console.log(this.cache);
+  }
+}
 
-    //         if (fs.lstatSync(namespaceDirectory).isDirectory()) {
-    //             console.log(`${namespaceDirectory} is a directory!`);
+let collectionFinder = new CollectionFinder(
+  "/var/home/goneri/git_repos/cloud-init-bsd-testing",
+);
+collectionFinder.refreshCache();
 
-    //             fs.readdir(namespaceDirectory, function (error, files) {
-    //                 console.log(`files: ${files}`)
+let collectionFinderHome = new CollectionFinder("/var/home/goneri/.ansible/");
+collectionFinderHome.refreshCache();
 
-    //             });
-    //         }
-
-    //     })
-    // .then((files) => )
-    // .catch((error) => {
-    //     return console.log('Unable to scan directory: ' + error);
-    // });
+let collectionFinderWorkspace = new CollectionFinder(
+  "/var/home/goneri/git_repos/cloud-init-bsd-testing/collections/ansible_collections/community/general",
+);
+collectionFinderWorkspace.refreshCache();
